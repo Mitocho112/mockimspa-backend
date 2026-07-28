bae106070f1d8702352e2228add518bd37c54cdc
USER <user@USERs-MacBook-Air.local>
2026-07-27 18:30:02 -0700

    initial setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+require('./db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Moc Kim Spa API running'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
