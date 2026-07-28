628e65966b89e72296072ce21959ba3aa4034536
USER <user@USERs-MacBook-Air.local>
2026-07-27 20:23:51 -0700

    fix server to include webhook route

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 require('./db');
 
+const webhookRouter = require('./routes/webhook');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Moc Kim Spa API running'));
+app.use('/webhook', webhookRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
