e6fea767bffec5c983b768cd1841f35d7b7fd4f0
USER <user@USERs-MacBook-Air.local>
2026-07-27 21:03:58 -0700

    add messages send route

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 require('./db');
 
 const webhookRouter = require('./routes/webhook');
+const messagesRouter = require('./routes/messages');
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Moc Kim Spa API running'));
 app.use('/webhook', webhookRouter);
+app.use('/api/messages', messagesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
