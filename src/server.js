322769ba91294134f894ffd8d4c5c6dc43c3d764
USER <user@USERs-MacBook-Air.local>
2026-07-30 03:47:21 -0700

    mount all API routes in server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ require('./db');
 const webhookRouter = require('./routes/webhook');
 const messagesRouter = require('./routes/messages');
 const authRouter = require('./routes/auth');
+const conversationsRouter = require('./routes/conversations');
+const contactsRouter = require('./routes/contacts');
+const bookingsRouter = require('./routes/bookings');
+const templatesRouter = require('./routes/templates');
+const usersRouter = require('./routes/users');
 
 const app = express();
 app.use(cors());
@@ -15,6 +20,11 @@ app.get('/', (req, res) => res.send('Moc Kim Spa API running'));
 app.use('/webhook', webhookRouter);
 app.use('/api/messages', messagesRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/conversations', conversationsRouter);
+app.use('/api/contacts', contactsRouter);
+app.use('/api/bookings', bookingsRouter);
+app.use('/api/templates', templatesRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
